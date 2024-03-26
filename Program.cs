cacc8f06abcdc55962c5276d38ea1f33060ff7f8
Modleyyy <zaki.amarouche.tralala@gmail.com>
2024-03-26 12:43:27 +0100

    Added --debug and -d CLI options for time profiling

--- Program.cs
+++ Program.cs
@@ -1,38 +1,73 @@
-﻿using Burg.FrontEnd.AST;
+﻿using System.Diagnostics;
+using Burg.FrontEnd.AST;
 using Burg.FrontEnd.Tokenizing;
 using Burg.Runtime;
 using Environment = Burg.Runtime.Environment;
+using SMV = Burg.Runtime.Interpreter.StmtReturnValue;
 
 public static class Program
 {
     public static string path = "require doesn't work in repl mode dummy";
     public static void Main(string[] args)
     {
+        bool debugMode = false;
+        int debugIndex = Array.FindIndex(args, arg => arg == "-d" || arg == "--debug");
+
+        if (debugIndex != -1)
+        {
+            debugMode = true;
+            args = args.Where((arg, index) => index != debugIndex).ToArray();
+        }
+
         if (args.Length > 0)
         {
             string path = args[0];
             Program.path = path;
             string code = File.ReadAllText(path);
 
-            ExecuteCode(code, new());
+            ExecuteCode(code, new(), debugMode);
         }
         else
         {
             Console.WriteLine("\tBurgR -- Burg REPL 1.0");
             Console.WriteLine("\tWrite 'exit' to leave");
-            REPL();
+            REPL(debugMode);
         }
     }
 
-    private static IRuntimeValue ExecuteCode(string code, Environment env)
+    private static SMV ExecuteCode(string code, Environment env, bool debugMode)
     {
-        List<Token> tokens = Tokenizer.Tokenize(code);
-        Chunk chunk = Parser.ParseAST(tokens);
+        Stopwatch sw = new();
+        if (debugMode)
+            sw.Start();
 
-        return Interpreter.Evaluate(chunk, env);
+        List<Token> tokens = Tokenizer.Tokenize(code);
+        if (debugMode)
+        {
+            sw.Stop();
+            Console.WriteLine($"Spent: {sw.ElapsedMilliseconds}ms tokenizing.");
+            sw.Restart();
+        }
+
+        Chunk chunk = Parser.ParseAST(tokens);
+        if (debugMode)
+        {
+            sw.Stop();
+            Console.WriteLine($"Spent: {sw.ElapsedMilliseconds}ms parsing.");
+            sw.Restart();
+        }
+
+        SMV value = Interpreter.EvaluateStmt(chunk, env);
+        if (debugMode)
+        {
+            sw.Stop();
+            Console.WriteLine($"Spent: {sw.ElapsedMilliseconds}ms interpreting.");
+        }
+
+        return value;
     }
 
-    private static void REPL()
+    private static void REPL(bool debugMode)
     {
         Environment env = new();
         while (true)
@@ -43,7 +78,7 @@ public static class Program
                 return;
 
             env = new(env);
-            Console.WriteLine(ExecuteCode(code, env).value);
+            Console.WriteLine(ExecuteCode(code, env, debugMode).value);
         }
     }
 }
